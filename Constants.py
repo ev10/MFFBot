72dbb2b176062e62c29c98998c850ac3ae5263d9
ev10 <53459963+ev10@users.noreply.github.com>
2019-11-27 21:13:35 -0500

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -4,4 +4,9 @@ TesseractPath = r"-\Tesseract-OCR\tesseract.exe"
 #верхний правый угол
 ScreenCapRange = {'top': 100, 'left': 100, 'width': 250, 'height': 250}
 
+ImagePath = r"C:\MFFIMG_ENG" + "\\"
+ImagePathRU = r"C:\MFFIMG_RUS" + "\\"
+
 SCTimeDelay = 0.15
+
+AuthToken = "discord account auth token here"
