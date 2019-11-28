8cf2fccaf291b4a5f230181baf1106474d81292a
ev10 <53459963+ev10@users.noreply.github.com>
2019-11-27 21:11:57 -0500

    Update Recognition.py

--- Recognition.py
+++ Recognition.py
@@ -1,1 +1,18 @@
 import pyautogui
+import Constants
+import time
+
+def locate(s):
+    acc = 0
+    while True:
+         acc = acc + 1
+         if (acc > 3):
+            return 0
+         im1 = pyautogui.screenshot()
+         coord = pyautogui.locateOnScreen(Constants.ImagePath + s, confidence = 0.8)
+         if (coord is not None):
+            buttonpoint = pyautogui.center(coord)
+            x, y = buttonpoint
+            time.sleep(0.5)
+            print(x,y)
+            return 1
